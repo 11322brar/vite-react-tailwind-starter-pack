da1bec33862494e7aae32abc5cbef513621715be
Arshdeep Singh <11322brar@gmail.com>
2024-05-29 18:00:55 +0530

    Adding files

---
+++ my-project/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum,
+        adipisci?
+      </p>
+    </div>
+  );
+}
+
+export default App;
